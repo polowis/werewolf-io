bf889ef7188f86394787ad767a91c2661490798b
polowis <tranphuochung0912@gmail.com>
2020-03-30 11:25:45 +1100

    fix: change white background to dark

--- resources/js/components/RoleComponent.vue
+++ resources/js/components/RoleComponent.vue
@@ -72,7 +72,7 @@ export default {
   width: 700px;
   margin: 0px auto;
   padding: 20px 30px;
-  background-color: #fff;
+  background-color: #343a40;
   border-radius: 2px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
   transition: all 0.3s ease;
